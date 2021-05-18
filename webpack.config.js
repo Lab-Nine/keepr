5fe787c719439ac2ac5b099281b225e05ebaf268
chris-paul-ejercito <chris.paul.ejercito@gmail.com>
2021-05-17 17:29:10 -0700

    add packages, including webpack, babel, react, eslint, express, passport, and postgres; configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: [
+          // Creates 'style' nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    port: 8080,
+    hot: true,
+    publicPath: "./build",
+    proxy: {
+      "/": "http://localhost:3000",
+    },
+  },
+};
