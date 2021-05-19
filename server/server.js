ba00e00db5cc409fe24f48120cecca9f217f64a4
chris-paul-ejercito <chris.paul.ejercito@gmail.com>
2021-05-18 22:46:50 -0700

    add cookieSession to server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
+
 const app = express();
 const path = require('path');
 require('./passport');
 
 const PORT = 3000;
 
+app.use(cookieSession({
+  name: 'session-name',
+  keys: ['key1', 'key2'],
+}));
+
 app.use(express.json());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -22,6 +30,7 @@ app.get('/auth/google',
     scope: [
       'https://www.googleapis.com/auth/userinfo.email',
     ],
+    successRedirect: '/',
   }));
 
 app.get('/auth/google/callback',
@@ -29,6 +38,15 @@ app.get('/auth/google/callback',
   (req, res) => {
     res.redirect('/');
   });
+
+// const checkUserLoggedIn = (req, res, next) => {
+//   console.log(req.user);
+//   req.user ? next() : res.sendStatus(401);
+// };
+
+// app.get('/profile', checkUserLoggedIn, (req, res) => {
+//   res.send(`<h1>${req.user}</h1>`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
